e6bd8d92c991186c70f76945bf1c24c7e9eaa552
Isuru Ranawaka <isu3ru@gmail.com>
2023-03-07 00:42:09 +0530

    auth routes added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Get the authenticated user
+     */
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+
+    /**
+     * Register a new user
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+    /**
+     * Login the user
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // check if the user exists
+        $user = User::where('email', $request->email)->first();
+
+        // if the user doesn't exist
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // create token for the user which expires in a week
+        $token = $user->createToken(name: 'auth_token', expiresAt: now()->addWeek())->plainTextToken;
+
+        // return the token
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Logout the user
+     */
+    public function logout(Request $request)
+    {
+        // revoke the token
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::name('api.')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        // unauthenticated routes only
+        Route::post('/register', 'register')->name('register');
+        Route::post('/login', 'login')->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+        // authenticated routes only
+        Route::middleware('auth:sanctum')->group(function () {
+            // user routes
+            Route::get('/user', 'user')->name('user');
+            Route::post('/logout', 'logout')->name('logout');
+        });
+    });
 });
